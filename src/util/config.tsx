5a8c72f238f55f4b4a95bde91007541d6513c387
jitae <jitaekim93@gmail.com>
2018-11-24 14:21:29 +0900

    add config file for global constants

---
+++ src/util/config.tsx
@@ -0,0 +1,30 @@
+import firebase from 'firebase';
+const config = {
+    apiKey: "AIzaSyBD9eX7ABB0Xu1N6CnSdKL-bnsNF5WgLtc",
+    authDomain: "chainchronicle-ea233.firebaseapp.com",
+    databaseURL: "https://chainchronicle-ea233.firebaseio.com",
+    projectId: "chainchronicle-ea233",
+    storageBucket: "chainchronicle-ea233.appspot.com",
+    messagingSenderId: "1008757887866"
+  };
+
+firebase.initializeApp(config);
+
+var isTestEnvironment;
+if (location.hostname === 'localhost') {
+    isTestEnvironment = true;
+}
+else {
+    isTestEnvironment = false;
+}
+
+export const BASE_REF = firebase.database().ref();
+export const ARCANA_REF = BASE_REF.child('arcana');
+export const DYNAMIC_LINK = 'https://q623s.app.goo.gl/';
+
+export const IS_TEST_ENVIRONMENT = isTestEnvironment;
+
+export const GAKEY = 'UA-70981211-1';
+
+// export const greenColor = '#68a283'
+
