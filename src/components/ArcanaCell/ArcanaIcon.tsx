9a32d8fb21114872a6edc9768bd39c25ffd21c9f
jitae <jitaekim93@gmail.com>
2018-11-26 09:42:29 +0900

    add autocomplete logic

---
+++ src/components/ArcanaCell/ArcanaIcon.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ArcanaIcon = styled.img`
+    margin: 10px;
+    height: 66px;
+    width: 66px;
+    /* float: left; */
+`;
+
+export default ArcanaIcon;
--- src/components/SearchBar.tsx/SearchBar.tsx
+++ src/components/SearchBar.tsx/SearchBar.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import Downshift from 'downshift';
-import { TextField, withStyles, Paper, MenuItem } from '@material-ui/core';
+import { TextField, withStyles, Paper, MenuItem, Typography } from '@material-ui/core';
 import styled from 'styled-components';
 import { PaperProps } from '@material-ui/core/Paper';
 import { inject, observer } from 'mobx-react';
 import RootStore from '../../stores/RootStore';
 import { BASE_REF } from '../../util/config';
+import ArcanaIcon from '../ArcanaCell/ArcanaIcon';
 
 const StyledPaper = styled(Paper as React.SFC<PaperProps>)`
     position: absolute;
@@ -31,21 +32,48 @@ class SearchBar extends React.Component<Props> {
         return this.props as InjectedProps;
     }
 
+    componentDidMount() {
+        // BASE_REF.child('arcana').once('value', snapshot => {
+        //     if (snapshot) {
+        //         let searchArray: any = {};
+        //         snapshot.forEach((child) => {
+        //             const key = child.key;
+        //             const arcana = child.val();
+        //             const searchArcana = {
+        //                 nameKR: arcana.nicknameKR + ' ' + arcana.nameKR,
+        //                 iconURL: arcana.iconURL || '',
+        //                 imageURL: arcana.imageURL || '',
+        //             }
+        //             searchArray[key!] = searchArcana;
+        //         });
+
+        //         BASE_REF.child('search').set(searchArray);
+        //     }
+        // })
+    }
+
     observeNames = () => {
 
         const { rootStore } = this.injected;
         const navBarStore = rootStore.navBarStore;
     
-        BASE_REF.child('name').once('value', snapshot => {
+        BASE_REF.child('search').once('value', snapshot => {
     
             let namesArray: any = [];
             if (snapshot) {
                 snapshot.forEach((child) => {
-                    const arcana = {
-                        arcanaID: child.key,
-                        nameKR: child.val()
+                    const key = child.key;
+                    if (key) {
+                        const arcana = child.val();
+                        const searchArcana = {
+                            arcanaID: key,
+                            nameKR: arcana.nameKR,
+                            iconURL: arcana.iconURL,
+                            imageURL: arcana.imageURL,
+                        }
+                        namesArray.push(searchArcana);
                     }
-                    namesArray.push(arcana);
+
                 })
             }
             navBarStore.setNamesArray(namesArray);
@@ -64,6 +92,10 @@ class SearchBar extends React.Component<Props> {
                 style={{flexGrow: 1}}
             >
                 <Downshift
+                    onSelect={(selectedItem: any, stateAndHelpers: object) => {
+                        const arcanaID = selectedItem.arcanaID;
+                        rootStore.routerStore.push(`arcana?arcana=${arcanaID}`);
+                    }}
                 >
                     {({
                         getInputProps,
@@ -90,9 +122,19 @@ class SearchBar extends React.Component<Props> {
                                     <StyledPaper
                                         square
                                     >
-                                        {navBarStore.getSearchResults().map((arcana: any) =>
-                                            <MenuItem>
-                                            {arcana.nameKR}
+                                        {navBarStore.getSearchResults().map((item: any, index) =>
+                                            <MenuItem
+                                                {...getItemProps({
+                                                key: item.arcanaID,
+                                                index,
+                                                item,
+                                                style: {
+                                                    height: '100%', paddingTop: 0, paddingBottom: 0
+                                                },
+                                              })}                                                
+                                            >
+                                                <ArcanaIcon src={item.iconURL}/>
+                                                <Typography>{item.nameKR}</Typography>
                                             </MenuItem>
                                         )}                            
                                     </StyledPaper>
