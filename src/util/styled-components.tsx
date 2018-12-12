99599743b51d1a6b6ce64c5912baf504832bd5e2
jitae <jitaekim93@gmail.com>
2018-12-11 22:18:38 -0800

    add media query helper

--- src/util/styled-components.tsx
+++ src/util/styled-components.tsx
@@ -11,5 +11,22 @@ const {
   ThemeProvider
 } = styledComponents as ThemedStyledComponentsModule<ThemeInterface>;
 
-export { css, createGlobalStyle, keyframes, ThemeProvider };
+const sizes: any = {
+  phone: 320,
+  tablet: 768,
+  desktop: 1024,
+}
+
+// Iterate through the sizes and create a media template
+const media = Object.keys(sizes).reduce((acc: any, label) => {
+  acc[label] = (...args: any) => css`
+    @media (min-width: ${sizes[label] / 16}em) {
+      ${css(args)}
+    }
+  `
+
+  return acc
+}, {})
+
+export { css, createGlobalStyle, keyframes, ThemeProvider, media };
 export default styled;
